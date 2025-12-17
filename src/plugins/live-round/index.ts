e93224aafa8dee402fcf9bb3a551891545775541
MRegterschot <marijn.regterschot@gmail.com>
2025-12-17 15:08:46 +0100

    fix: remove debug log from rounds filtering

--- src/plugins/live-round/index.ts
+++ src/plugins/live-round/index.ts
@@ -259,8 +259,6 @@ export default class LiveRoundPlugin extends Plugin {
       }
     }
 
-    console.log(this.rounds);
-
     this.rounds = this.rounds.filter(
       (r) => !this.clientManager.isReverseCupMode() || r.points > -2000,
     );
