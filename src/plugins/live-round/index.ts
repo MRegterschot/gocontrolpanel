5fef64a3e265f4b2c11c40a584a0841ec2f591d3
MRegterschot <marijn.regterschot@gmail.com>
2026-04-03 23:45:28 +0200

    feat: enhance live round widget with personal best and record tracking
    - Added new fields to the Finish struct for isPersonalBest, isLocalRecord, and isWorldRecord.
    - Updated live-round.hbs to display record indicators for players.
    - Modified updateFinish function to handle new record types and display them appropriately.
    - Implemented logic to fetch and update personal bests, local records, and world records for players.
    - Enhanced the clearLiveRound and updateWidget methods to ensure accurate record tracking and display.

--- src/plugins/live-round/index.ts
+++ src/plugins/live-round/index.ts
@@ -1,12 +1,17 @@
+import { getLocalRecord } from "@/actions/database/server-only/records";
+import { getMapLeaderboard, getMapRecordsByAccounts } from "@/lib/api/nadeo";
+import { logger } from "@/lib/logger";
 import { GbxClientManager } from "@/lib/managers/gbxclient-manager";
 import ManialinkManager from "@/lib/managers/manialink-manager";
 import Widget from "@/lib/manialink/components/widget";
 import { getSpectatorStatus } from "@/lib/utils";
+import { SMapInfo } from "@/types/gbx/map";
 import { SPlayerInfo } from "@/types/gbx/player";
 import { Scores } from "@/types/gbx/scores";
 import { Waypoint, WaypointEvent } from "@/types/gbx/waypoint";
 import { LiveInfo, PlayerRound } from "@/types/live";
 import { PlayerInfo } from "@/types/player";
+import slugid from "slugid";
 import Plugin from "..";
 
 type Round = {
@@ -21,17 +26,32 @@ type Round = {
 type Finish = {
   login: string;
   points: number;
+  isPersonalBest: boolean;
+  isLocalRecord: boolean;
+  isWorldRecord: boolean;
+};
+
+type RecordsInfo = {
+  worldRecord: number;
+  localRecord: number;
+  [key: string]: number;
 };
 
 export default class LiveRoundPlugin extends Plugin {
   static pluginId = "live-round";
   static gamemodes = ["rounds", "cup", "reversecup"];
   private widget: Widget;
+  private liveFastestTime: number | null = null;
 
   private rounds: Round[] = [];
   private finishes: Finish[] = [];
   private pointsLimit: number = -1;
   private pointsRepartition: number[] = [];
+
+  private recordsInfo: RecordsInfo = {
+    worldRecord: 0,
+    localRecord: 0,
+  };
 
   constructor(
     clientManager: GbxClientManager,
@@ -69,9 +89,27 @@ export default class LiveRoundPlugin extends Plugin {
   async onStart() {
     this.widget.display();
     this.clearLiveRound();
+    this.updateRecordsInfo();
   }
 
   async onPlayerConnect(playerInfo: PlayerInfo) {
+    const map: SMapInfo =
+      await this.clientManager.client.call("GetCurrentMapInfo");
+
+    try {
+      const personalBest = await getMapRecordsByAccounts(map.UId, [
+        slugid.decode(playerInfo.login),
+      ]);
+
+      this.recordsInfo[playerInfo.login] = personalBest[0]
+        ? personalBest[0].recordScore.time
+        : 0;
+    } catch (error) {
+      logger.error(error, "Error fetching personal best for player connect");
+      this.recordsInfo[playerInfo.login] =
+        this.recordsInfo[playerInfo.login] || 0;
+    }
+
     const playerStatus = this.clientManager.reverseCupGetPlayerStatus(
       playerInfo.login,
     );
@@ -156,7 +194,9 @@ export default class LiveRoundPlugin extends Plugin {
   }
 
   async onBeginMap() {
+    this.liveFastestTime = null;
     this.clearLiveRound();
+    this.updateRecordsInfo();
   }
 
   async onBeginMatch() {
@@ -187,39 +227,53 @@ export default class LiveRoundPlugin extends Plugin {
   async onFinish(finish: Waypoint) {
     if (this.clientManager.info.liveInfo.isWarmUp) return;
 
+    if (finish.racetime === 0) return;
+
+    let isFastest = false;
+    if (
+      this.liveFastestTime === null ||
+      finish.racetime < this.liveFastestTime
+    ) {
+      this.liveFastestTime = finish.racetime;
+      isFastest = true;
+    }
+
     for (let i = 0; i < this.rounds.length; i++) {
       if (this.rounds[i].login === finish.login) {
         this.rounds[i].time = finish.racetime;
         this.rounds[i].checkpoints = finish.curracecheckpoints;
 
-        const position = Math.min(
-          this.finishes.length,
-          this.pointsRepartition.length - 1,
-        );
-        let points = this.pointsRepartition[position] || 0;
-
-        if (this.clientManager.info.liveInfo.type === "reversecup") {
-          const playerCount = this.rounds.filter(
-            (r) => r.points > -2000,
-          ).length;
-
-          const repartition =
-            this.clientManager.reverseCupGetPointsRepartition(playerCount);
-
-          const position = Math.min(
-            this.finishes.length,
-            repartition.length - 1,
-          );
-
-          points = -repartition[position] || 0;
-        }
-
         this.finishes.push({
           login: finish.login,
-          points,
+          points: 0,
+          isPersonalBest:
+            this.recordsInfo[finish.login] > 0 &&
+            finish.racetime < this.recordsInfo[finish.login],
+          isLocalRecord:
+            isFastest && this.fasterThanLocalRecord(finish.racetime),
+          isWorldRecord:
+            isFastest && this.fasterThanWorldRecord(finish.racetime),
         });
 
         await this.updateWidget();
+
+        if (
+          this.recordsInfo[finish.login] === 0 ||
+          finish.racetime < this.recordsInfo[finish.login]
+        ) {
+          this.recordsInfo[finish.login] = finish.racetime;
+        }
+
+        if (isFastest) {
+          if (this.fasterThanLocalRecord(finish.racetime)) {
+            this.recordsInfo.localRecord = finish.racetime;
+          }
+
+          if (this.fasterThanWorldRecord(finish.racetime)) {
+            this.recordsInfo.worldRecord = finish.racetime;
+          }
+        }
+
         return;
       }
     }
@@ -271,6 +325,64 @@ export default class LiveRoundPlugin extends Plugin {
     );
 
     await this.updateWidget();
+  }
+
+  async updateRecordsInfo() {
+    const map: SMapInfo =
+      await this.clientManager.client.call("GetCurrentMapInfo");
+
+    const players = this.clientManager.info.activePlayers
+      .map((p) => p.login)
+      .filter((login) => !login.includes("fakeplayer"));
+
+    if (!map) {
+      this.recordsInfo = {
+        worldRecord: 0,
+        localRecord: 0,
+      };
+
+      players.forEach((p) => {
+        this.recordsInfo[p] = 0;
+      });
+    } else {
+      const localRecord = await getLocalRecord(
+        this.clientManager.getServerId(),
+        map.UId,
+      );
+
+      this.recordsInfo.localRecord = localRecord ? localRecord.time : 0;
+
+      const onlineLeaderboard = await getMapLeaderboard(map.UId);
+      if (
+        onlineLeaderboard.tops.length > 0 &&
+        onlineLeaderboard.tops[0].top.length > 0
+      ) {
+        const wr = onlineLeaderboard.tops[0].top[0];
+
+        this.recordsInfo.worldRecord = wr.score;
+      } else {
+        this.recordsInfo.worldRecord = 0;
+      }
+
+      try {
+        const personalBests = await getMapRecordsByAccounts(
+          map.UId,
+          players.map((p) => slugid.decode(p)),
+        );
+
+        players.forEach((p) => {
+          const pb = personalBests.find(
+            (r) => r.accountId === slugid.decode(p),
+          );
+          this.recordsInfo[p] = pb ? pb.recordScore.time : 0;
+        });
+      } catch (error) {
+        logger.error(error, "Error fetching personal bests");
+        players.forEach((p) => {
+          this.recordsInfo[p] = this.recordsInfo[p] || 0;
+        });
+      }
+    }
   }
 
   async clearLiveRound() {
@@ -353,6 +465,59 @@ export default class LiveRoundPlugin extends Plugin {
       this.rounds[i].rank = i + 1;
     }
 
+    // Make sure finishes are sorted correctly in the same order as rounds
+    this.finishes.sort((a, b) => {
+      const roundA = this.rounds.find((r) => r.login === a.login);
+      const roundB = this.rounds.find((r) => r.login === b.login);
+
+      if (!roundA || !roundB) return 0;
+
+      return roundA.rank - roundB.rank;
+    });
+
+    // Make sure to update points for finishes based on their position
+    for (let i = 0; i < this.finishes.length; i++) {
+      const finish = this.finishes[i];
+      const round = this.rounds.find((r) => r.login === finish.login);
+      if (!round) continue;
+
+      const position = Math.min(i, this.pointsRepartition.length - 1);
+      let points = this.pointsRepartition[position] || 0;
+
+      if (this.clientManager.info.liveInfo.type === "reversecup") {
+        const playerCount = this.rounds.filter((r) => r.points > -2000).length;
+
+        const repartition =
+          this.clientManager.reverseCupGetPointsRepartition(playerCount);
+
+        const position = Math.min(i, repartition.length - 1);
+
+        points = -repartition[position] || 0;
+      }
+
+      this.finishes[i].points = points;
+    }
+
+    // Make sure only the first finish has the world/local record if it's faster than the current ones
+    if (this.finishes.length > 0) {
+      const firstFinish = this.finishes[0];
+      const round = this.rounds.find((r) => r.login === firstFinish.login);
+
+      if (round) {
+        firstFinish.isLocalRecord =
+          this.fasterThanLocalRecord(round.time) && round.time > 0;
+        firstFinish.isWorldRecord =
+          this.fasterThanWorldRecord(round.time) && round.time > 0;
+      }
+    }
+
+    if (this.finishes.length > 1) {
+      for (let i = 1; i < this.finishes.length; i++) {
+        this.finishes[i].isLocalRecord = false;
+        this.finishes[i].isWorldRecord = false;
+      }
+    }
+
     this.widget.setData({
       roundsJson: JSON.stringify(this.rounds),
       finishesJson: JSON.stringify(this.finishes),
@@ -360,5 +525,15 @@ export default class LiveRoundPlugin extends Plugin {
       pointsLimit: this.pointsLimit,
     });
     this.widget.update();
+  }
+
+  private fasterThanLocalRecord(time: number) {
+    return (
+      this.recordsInfo.localRecord === 0 || time < this.recordsInfo.localRecord
+    );
+  }
+
+  private fasterThanWorldRecord(time: number) {
+    return time < this.recordsInfo.worldRecord;
   }
 }
