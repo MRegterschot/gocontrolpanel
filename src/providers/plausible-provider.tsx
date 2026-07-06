3a3914ed050ed7c92d794f403ff6ed982783026c
MRegterschot <marijn.regterschot@gmail.com>
2026-07-06 19:06:11 +0200

    feat: add PLAUSIBLE_DOMAIN to environment variables and update PlausibleProvider to use it

--- src/providers/plausible-provider.tsx
+++ src/providers/plausible-provider.tsx
@@ -5,14 +5,16 @@ export default function PlausibleProvider({
 }: {
   children: React.ReactNode;
 }) {
+  const domain = process.env.PLAUSIBLE_DOMAIN;
   const apiHost = process.env.PLAUSIBLE_API_HOST;
 
-  if (!apiHost) {
+  if (!apiHost || !domain) {
     return <>{children}</>;
   }
 
   return (
     <NextPlausibleProvider
+      data-domain={domain}
       src={`https://${apiHost}/js/script.file-downloads.hash.outbound-links.pageview-props.revenue.tagged-events.js`}
     >
       {children}
